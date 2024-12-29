67242e2864b65345112091c9c0ade22462905d34
Давид <manvelyancda@masterscada.ru>
2024-12-29 15:15:47 +0300

    refactoring

--- Program.cs
+++ Program.cs
@@ -12,56 +12,16 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var offset = 0;
+        var tgBotPollPeriodInSeconds = TimeSpan.FromSeconds(double.TryParse(Environment.GetEnvironmentVariable("TG_BOT_POLL_PERIOD_SECONDS"), out var _tgBotPollPeriodInSeconds) ? _tgBotPollPeriodInSeconds : 10d);
         var apiKey = Environment.GetEnvironmentVariable("API_KEY")!;
-        var telegramBot = new TelegramBotClient(apiKey);
-        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
         Console.WriteLine("bot run!");
 
-        while (true)
-        {
-            var updates = await telegramBot.GetUpdatesAsync(offset);
-            await Task.Delay(TimeSpan.FromSeconds(5));
-
-            foreach (var update in updates)
-            {
-                if (update is null || update.Message is null)
-                    continue;
-
-                if (update.Message.Type == MessageType.Text)
-                {
-                    if (string.IsNullOrEmpty(update.Message.Text))
-                        return;
-
-                    var generator = new QRCodeGenerator();
-                    var encodedData = Encoding.UTF8.GetBytes(update.Message.Text);
-                    var qrData = generator.CreateQrCode(encodedData, QRCodeGenerator.ECCLevel.L);
-                    var qrCode = new PngByteQRCode(qrData).GetGraphic(5);
-                    var inputFile = InputFile.FromStream(new MemoryStream(qrCode), "qrCode.png");
-
-                    await telegramBot.SendPhotoAsync(update.Message.Chat, inputFile);
-                }
-
-                if (update.Message.Type == MessageType.Photo)
-                {
-                    if (update.Message.Photo == null)
-                        return;
-
-                    var photo = update.Message.Photo.Last();
-
-                    await using (var ms = new MemoryStream())
-                    {
-                        var file = await telegramBot.GetInfoAndDownloadFileAsync(photo.FileId, ms);
-                        var btm = SKBitmap.Decode(ms.ToArray());
-                        var reader = new ZXing.SkiaSharp.BarcodeReader();
-                        var result = reader.Decode(btm);
-
-                        await telegramBot.SendTextMessageAsync(update.Message.Chat, result?.Text ?? "QR code is not identified");
-                    }
-                }
-
-                offset = update.Id + 1;
-            }
-        }
+        await Task.WhenAll([
+                new TelegramBot().RunAsync(
+                    apiKey,
+                    tgBotPollPeriodInSeconds,
+                    CancellationToken.None)
+            ]);
     }
 }
